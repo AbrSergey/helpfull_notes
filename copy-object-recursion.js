7e7c443bf59e1f2a22543bf97fce8ba61567f592
AbrSergey <AbrSergey@users.noreply.github.com>
2019-05-29 19:32:24 +0700

    Create copy-object-recursion.js

---
+++ copy-object-recursion.js
@@ -0,0 +1,12 @@
+//recursion copy object (maybe array)
+const copy = function copyRecursive(o){
+  let output, v, key;
+  output = Array.isArray(o) ? [] : {};
+  for (key in o) {
+    v = o[key];
+    output[key] = (typeof v === "object") ? copyRecursive(v) : v;
+  }
+  return output;
+};
+
+module.exports.copy = copy;
